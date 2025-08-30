fb3a7aceab6c38de9d41a7a5c33f6f280e71b837
aleksardo <sahar_bo@mail.ru>
2025-08-30 10:36:19 +0300

    слив в гитхаб

--- calc/form_calc.py
+++ calc/form_calc.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 class CalcForm(forms.Form):
-    Lb=forms.DecimalField(label='Lb', initial=5, min_value=1.5, decimal_places=1)
-    Wb=forms.DecimalField(label='Wb', initial=3, min_value=1, decimal_places=1)
-    Hb=forms.DecimalField(label='Hb', initial=4, min_value=2, decimal_places=1)
+    Lb=forms.DecimalField(label='Даўжыня будынка - Lb, м',
+                           initial=5, min_value=1.5, decimal_places=1,
+                           widget= forms.NumberInput(attrs={"class": "inpt_ed"}))
+    Wb=forms.DecimalField(label='Шырыня будынка - Wb, м', 
+                          initial=3, min_value=1, decimal_places=1,
+                          widget= forms.NumberInput(attrs={"class": "inpt_ed"}))
+    Hb=forms.DecimalField(label='Вышыня будынка - Hb, м',
+                           initial=4, min_value=2, decimal_places=1, 
+                           widget= forms.NumberInput(attrs={"class": "inpt_ed"}))
     
 
 
